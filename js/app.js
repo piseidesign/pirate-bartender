f875f72a5c09786ae3e86c4379c64f842d3aca9b
Pisei <limpisei@gmail.com>
2016-11-14 12:09:48 -0800

    refactored index

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-// var questions = {
-//   strong: 'Do ye like yer drinks strong?',
-//   salty: 'Do ye like it with a salty tang?'
-// }
-// var ingredients = {
-//   strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
-//   salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon']
-// }
-
 var Ingredients = function(options) {
   this.options = options;
 };
@@ -20,19 +11,16 @@ var Pantry = function(stock) {
 
 var newPantry = new Pantry(['water', 'ginger ale', 'juice']);
 
-var Questions = function(type) {
-  this.type = type;
-}
-
-var strongQuestion = new Questions('Do ye like yer drinks strong?');
-var saltyQuestion = new Questions('Do ye like it with a salty tang?');
-
-var questionsArray = [strongQuestion.type, saltyQuestion.type];
+// var Questions = function(type) {
+//   this.type = type;
+// }
+//
+// var strongQuestion = new Questions('Do ye like yer drinks strong?');
+// var saltyQuestion = new Questions('Do ye like it with a salty tang?');
+//
+// var questionsArray = [strongQuestion.type, saltyQuestion.type];
 
 $(function() {
-  $('.question-wrapper').append('<div class="strong-question">' + questionsArray[0] + '<br><button class="strong-yes">Yes</button><button class="strong-no">No</button></div>')
-
-
   var includedItems = [];
 
   $('.strong-yes').click(function() {
